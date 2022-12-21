e5fc1b8e17c2bbde325778755b978ebef0c665cb
marseleon <marselinusleonardowillyam@gmail.com>
2022-12-21 15:04:27 +0700

    Tugas10

---
+++ app/src/main/java/com/example/pertemuan5_recycleview/ProfileActivity.java
@@ -0,0 +1,30 @@
+package com.example.pertemuan5_recycleview;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import com.example.pertemuan5_recycleview.databinding.ActivityProfileBinding;
+public class ProfileActivity extends AppCompatActivity {
+    private ActivityProfileBinding binding;
+    SharedPreferences preferences;
+    SharedPreferences.Editor editor;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setup view binding
+        binding = ActivityProfileBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        preferences = getSharedPreferences("AndroidHiveLogin",
+                0);
+        editor = preferences.edit();
+        binding.buttonLogOut.setOnClickListener(new
+                                                        View.OnClickListener() {
+                                                            @Override
+                                                            public void onClick(View v) {
+
+                                                            }
+                                                        });
+    }
+}
